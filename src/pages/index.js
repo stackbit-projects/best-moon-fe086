b36adc1d0e701b1389b9777abc9f21bee28d8c7b
Stackbit Code Editor <support@stackbit.com>
2021-03-03 19:25:31 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,16 +31,7 @@ const IndexPage = () => (
           </div>
           <div className="col-lg-5 my-auto">
             <img src='http://artnerd.net/src/assets/main/portrait.png' className="img-fluid" alt="" />
-            <div className="device-container">
-              <div className="device-mockup iphone6_plus portrait white">
-                {/* <div className="device"> */}
-                  {/* <div className="screen"> */}
-                    {/* <img src={'http://artnerd.net/src/assets/main/portrait.png'} className="img-fluid" alt="" /> */}
-                  {/* </div> */}
-                  {/* <div className="button"></div> */}
-                {/* </div> */}
-              </div>
-            </div>
+           
           </div>
         </div>
       </div>
